6b4ef7e8577e2291b087dd330a0e5a26d11b9775
VadimElFamoso <vadimprofessionel@gmail.com>
2026-01-23 16:42:35 +0100

    feat: continuation homepage

--- _dev/js/theme.js
+++ _dev/js/theme.js
@@ -3,6 +3,12 @@
  * ES6 code compiled to ES5 with esbuild
  */
 
+// Import Bootstrap 5
+import * as bootstrap from 'bootstrap';
+
+// Make Bootstrap available globally
+window.bootstrap = bootstrap;
+
 // Import your CSS/SCSS files here
 import '../css/theme.scss';
 
